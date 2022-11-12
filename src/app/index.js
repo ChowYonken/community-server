d1e969cc9fcc38e18ead3346fb912b91da8497f1
chowyonken <chowyonken@163.com>
2022-11-12 16:02:31 +0800

    解决跨域问题

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,14 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const useRoutes = require("../router");
 const errorHandler = require("./error-handle");
+const cors = require("koa-cors");
 
 const app = new Koa();
 
 app.useRoutes = useRoutes;
 
 app.use(bodyParser());
+app.use(cors());
 app.useRoutes();
 app.on("error", errorHandler);
 
